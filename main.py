3959c253f40d3ca2f43f78e7740b2b05c6aaf3ff
Juergen Pfundt <juergen.pfundt@gmail.com>
2024-10-03 15:22:07 +0200

    added SSD1306 spi oled to display status

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import time
 
 import micropython
-from machine import Pin, Timer
+from machine import Pin, Timer, SPI
+
+from ssd1306 import SSD1306_SPI
+
+
+class SPI_Display:
+    OFF = 0
+    ON = 1
+
+    def __init__(self) -> None:
+        # GPIO pins for attached oled spi display
+        # mosi: pin 7
+        # sck:  pin 6
+        # dc:   pin 8
+        # res:  pin 9
+        # cs:   pin 5
+
+        spi = SPI(0, 100000, mosi=Pin(7), sck=Pin(6))
+        self.ssd1306_spi = SSD1306_SPI(128, 64, spi, Pin(8), Pin(9), Pin(5))
+        self.ssd1306_spi.init_display()
+        self.hello()
+
+    def hello(self) -> None:
+        self.ssd1306_spi.fill(0)
+        self.ssd1306_spi.show()
+        self.ssd1306_spi.fill_rect(0, 0,  128,  16, 2)
+        self.ssd1306_spi.text("beck-view", 0, 0, 0)
+        self.ssd1306_spi.text("projector", 0, 8, 0)
+        self.ssd1306_spi.text("Press button", 0, 25)
+        self.ssd1306_spi.text("to start", 0, 35)
+        self.ssd1306_spi.text("projector", 0, 45)
+        self.ssd1306_spi.show()
 
 
 class Projector:
     OFF = 0
     ON = 1
+
+    def __init__(self, display: SPI_Display) -> None:
+        self.display = display
 
     # Define the GPIO pins for the projector's components
     led_pin = Pin(25, Pin.OUT, value=OFF)  # LED indicator for operation
@@ -41,7 +75,7 @@ class Projector:
     def send_ok1_signal(self, timer) -> None:
         """Send OK1 signal (frame advancement) every timer tick."""
         current_time = time.ticks_ms()
-        print(f"Time diff: {time.ticks_diff(current_time, self.start_time)} ms, Frame count: {self.count}")
+        time_diff = time.ticks_diff(current_time, self.start_time)
         self.start_time = current_time  # Update start_time for next frame
 
         # Flash the LED and toggle the OK1 pin to simulate a frame advancing signal
@@ -50,6 +84,13 @@ class Projector:
         time.sleep_us(8000)  # Wait for 8 milliseconds (8000 microseconds)
         self.ok1_pin.value(self.ON)  # Reset OK1 signal
         self.led_pin.value(self.OFF)  # Turn LED OFF
+
+        self.display.ssd1306_spi.fill(0)
+        self.display.ssd1306_spi.show()
+        self.display.ssd1306_spi.text(f"Frame {self.count}", 0, 0)
+        fps: float = 1000.0 / time_diff
+        self.display.ssd1306_spi.text(f"FPS   {fps:.2f}", 0, 10)
+        self.display.ssd1306_spi.show()
 
         self.count += 1  # Increment the frame counter
 
@@ -71,15 +112,19 @@ class Projector:
         time.sleep_us(8000)  # Wait for 8 milliseconds (8000 microseconds)
         self.eof_pin.value(self.ON)  # Reset EOF signal
         self.led_pin.value(self.OFF)  # Turn LED OFF
-        print(f"EOF reached. Total frames: {self.count}")
+        self.display.ssd1306_spi.fill(0)
+        self.display.ssd1306_spi.show()
+        self.display.ssd1306_spi.text(f"EOF reached", 0, 0)
+        self.display.ssd1306_spi.text(f"Total frames {self.count}", 0, 10)
+        self.display.ssd1306_spi.show()
         self.count = -1  # Reset frame counter
 
 
 if __name__ == '__main__':
     # Initialize the projector
-    projector = Projector()
+    spi_display = SPI_Display()
+    projector = Projector(spi_display)
     projector.led_pin.value(projector.ON)
-    print('<<<beck-view-projector initialized>>>')
 
     # Set up interrupts for the start and stop buttons (falling edge detection)
     projector.start_pin.irq(handler=projector.start_pin_isr, trigger=Pin.IRQ_FALLING)
